9c92d5ce2cf8b0c1ed2a4389fa0bb6af471eb550
Jeremy Echols <jechols@uoregon.edu>
2018-06-26 10:15:04 -0700

    Store user on request context
    This lets us query a database for user info only once per request
    
    - Uses gorilla/context for association of request-specific data
    - Pulls user data on all non-static requests
    - Updates static log handler to show IP and raw user login instead of
      potentially adding an extra DB hit per asset

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/uoregon-libraries/student-course-integrator
 
 require (
+	github.com/gorilla/context v1.1.1
 	github.com/gorilla/mux v1.6.2
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/uoregon-libraries/gopkg v0.0.0-20180626003652-57c8bc2c97e3
--- src/cmd/sci/middleware.go
+++ src/cmd/sci/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gorilla/context"
 	"github.com/uoregon-libraries/gopkg/logger"
 	"github.com/uoregon-libraries/student-course-integrator/src/data/user"
 	"github.com/uoregon-libraries/student-course-integrator/src/statusrecorder"
@@ -26,12 +27,32 @@ func getIP(req *http.Request) string {
 	return addr
 }
 
-// getUser uses the X-Remote-User header to find the user in our database, and
+func getRemoteUser(req *http.Request) string {
+	var u = req.Header.Get("X-Remote-User")
+	if u == "" {
+		return "N/A"
+	}
+	return u
+}
+
+// findUser uses the X-Remote-User header to find the user in our database, and
 // associates the current IP address
-func getUser(req *http.Request) *user.User {
-	var login = req.Header.Get("X-Remote-User")
+func findUser(req *http.Request) *user.User {
+	var login = getRemoteUser(req)
 	var user = user.Find(login)
 	user.IP = getIP(req)
+	return user
+}
+
+// getContextUser returns the user retrieved from the request context
+func getContextUser(req *http.Request) *user.User {
+	var data = context.Get(req, "user")
+	var user, ok = data.(*user.User)
+	if !ok {
+		// If this happens, there's a major logic error somewhere
+		panic("unable to read 'user' from context")
+	}
+
 	return user
 }
 
@@ -39,7 +60,7 @@ func requestLog(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		var sr = statusrecorder.New(w)
 		next.ServeHTTP(sr, req)
-		logger.Infof("Request: [%s] %s - %d", getUser(req), req.URL, sr.Status)
+		logger.Infof("Request: [%s] %s - %d", getContextUser(req), req.URL, sr.Status)
 	})
 }
 
@@ -47,13 +68,24 @@ func requestStaticAssetLog(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		var sr = statusrecorder.New(w)
 		next.ServeHTTP(sr, req)
-		logger.Debugf("Asset Request: [%s] %s - %d", getUser(req), req.URL, sr.Status)
+		var login = getRemoteUser(req)
+		logger.Debugf("Asset Request: [%s - %s] %s - %d", login, getIP(req), req.URL, sr.Status)
 	})
 }
 
-func fakeUser(next http.Handler) http.Handler {
+func fakeUserLogin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		req.Header.Set("X-Remote-User", "dummyuser")
+		next.ServeHTTP(w, req)
+	})
+}
+
+// getUser pulls the user from our data/user package and stores it on the
+// request via context
+func getUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		var user = findUser(req)
+		context.Set(req, "user", user)
 		next.ServeHTTP(w, req)
 	})
 }
@@ -62,7 +94,7 @@ func fakeUser(next http.Handler) http.Handler {
 // 403 is returned
 func mustAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		var user = getUser(req)
+		var user = getContextUser(req)
 		if !user.Authorized {
 			w.WriteHeader(http.StatusForbidden)
 			insufficientPrivileges.Execute(w, nil)
