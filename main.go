2194ac97ba69150e552068deb30bd841e065d4c6
Jeremy Echols <jechols@uoregon.edu>
2018-07-09 16:24:28 -0700

    Centralize configuration / globals setup
    Makes subcommands read and process config in a consistent way

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
+	"path/filepath"
 	"sort"
 	"strings"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/sessions"
+	flags "github.com/jessevdk/go-flags"
+	"github.com/uoregon-libraries/gopkg/fileutil"
+	"github.com/uoregon-libraries/gopkg/logger"
+	"github.com/uoregon-libraries/student-course-integrator/src/config"
+	"github.com/uoregon-libraries/student-course-integrator/src/global"
+	"github.com/uoregon-libraries/student-course-integrator/src/importcsv"
 	"github.com/uoregon-libraries/student-course-integrator/src/sci-server"
 )
 
@@ -34,5 +44,57 @@ func main() {
 	}
 
 	os.Args = append([]string{os.Args[0]}, os.Args[2:]...)
+	getConf()
 	fn()
+}
+
+func getConf() {
+	var p = flags.NewParser(&global.Opts, flags.HelpFlag|flags.PassDoubleDash)
+	var _, err = p.Parse()
+
+	if err != nil {
+		var flagErr, ok = err.(*flags.Error)
+		if !ok || flagErr.Type != flags.ErrHelp {
+			fmt.Fprintf(os.Stderr, "Error: %s\n\n", err)
+		}
+		p.WriteHelp(os.Stderr)
+		os.Exit(1)
+	}
+
+	if global.Opts.Approot == "" {
+		global.Opts.Approot, err = os.Getwd()
+		if err != nil {
+			logger.Fatalf("Error trying to read current directory: %s", err)
+		}
+	}
+
+	var files = []string{"/etc/sci.conf", filepath.Join(global.Opts.Approot, "sci.conf")}
+	if global.Opts.ConfigFile == "" {
+		for _, file := range files {
+			if fileutil.Exists(file) {
+				global.Opts.ConfigFile = file
+			}
+		}
+	}
+
+	if global.Opts.ConfigFile == "" {
+		logger.Fatalf("Config error: no config file found in %s", strings.Join(files, ", "))
+	}
+
+	global.Conf, err = config.Parse(global.Opts.ConfigFile)
+	if err != nil {
+		logger.Fatalf("Config error: %s", err)
+	}
+
+	if global.Conf.Debug {
+		logger.Warnf("Debug mode has been enabled")
+	}
+
+	global.DB, err = sql.Open("mysql", global.Conf.DatabaseConnect)
+	if err != nil {
+		logger.Fatalf("Error trying to connect to database: %s", err)
+	}
+	global.DB.SetConnMaxLifetime(time.Second)
+
+	global.Store = sessions.NewCookieStore([]byte(global.Conf.SessionSecret))
 }
--- src/global/global.go
+++ src/global/global.go
@@ -18,3 +18,8 @@ var DB *sql.DB
 
 // Store is our session storage backend
 var Store sessions.Store
+
+var Opts struct {
+	Approot    string `short:"r" long:"app-root" description:"path to app root if not current working directory"`
+	ConfigFile string `short:"c" long:"config" description:"path to SCI config file if not /etc/sci.conf or ./sci.conf"`
+}
