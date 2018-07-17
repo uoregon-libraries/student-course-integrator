d11abe54a803a3ab4061ab7dfb27642240af8eb0
Jeremy Echols <jechols@uoregon.edu>
2018-07-17 10:40:08 -0700

    Add a "real" get function

--- src/translator/get.go
+++ src/translator/get.go
@@ -3,7 +3,25 @@ package translator
 import (
 	"fmt"
 	"hash/crc32"
+	"io/ioutil"
+	"net/http"
 )
+
+// get aliases the getter function.  TODO: this alias should only be the real
+// getter except when testing.
+var get = _getMock
+
+// _getReal is a simple wrapper for REST API Get hits
+func _getReal(url string) (content []byte, err error) {
+	var resp *http.Response
+	resp, err = http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
+}
 
 // mockResponseTemplate MUST match the actual service's JSON if we want to
 // trust the mock service call
@@ -18,8 +36,8 @@ var mockResponseTemplate = `
 	}
 `
 
-// get is a simple wrapper for fake REST API Get hits
-func get(url string) (content []byte, err error) {
+// _getMock is a simple wrapper for fake REST API Get hits
+func _getMock(url string) (content []byte, err error) {
 	var idnum = crc32.ChecksumIEEE([]byte(url))%uint32(5) + 1
 	var response = fmt.Sprintf(mockResponseTemplate, idnum, idnum, "no message", 200)
 	return []byte(response), nil
