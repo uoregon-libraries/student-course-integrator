4f860a64472f937ef1131259b3c568b3e1244171
Jeremy Echols <jechols@uoregon.edu>
2018-08-01 15:07:17 -0700

    Expose version.Version as a function, not a string

--- src/version/version.go
+++ src/version/version.go
@@ -1,5 +1,9 @@
 package version
 
-// Version is displayed on web pages so we can be sure dev/staging/prod are the
+// version is displayed on web pages so we can be sure dev/staging/prod are the
 // version we expect them to be
-const Version = "1.0.0"
+const version = "1.0.0"
+
+func Version() string {
+	return version
+}
