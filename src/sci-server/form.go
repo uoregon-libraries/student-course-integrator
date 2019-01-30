2c84c11df115a98ca402d0019bb2086790d14aaf
lsat12357 <lsato@uoregon.edu>
2019-01-30 14:53:57 -0800

    sci-server: adding to form struct

--- src/sci-server/form.go
+++ src/sci-server/form.go
@@ -14,15 +14,17 @@ import (
 // based on the user filling out the form and the form fields themselves
 type form struct {
 	// Actual form fields: CRN and DuckID
-	CRN     string // CRN holds the submitted dropdown value for the selected course, e.g., "201704.X3159"
-	DuckID  string // DuckID holds the submitted duckid of the user being added, e.g., "echjeremy"
-	Confirm string // Confirm is "1" when the form is re-submitted after confirmation of the GE by name
-	Role    string // Role holds the submitted dropdown value for the selected role, e.g., "GE"
+	CRN             string // CRN holds the submitted dropdown value for the selected course, e.g., "201704.X3159"
+	DuckID          string // DuckID holds the submitted duckid of the user being added, e.g., "echjeremy"
+	Confirm         string // Confirm is "1" when the form is re-submitted after confirmation of the Agent by name
+	Role            string // Role holds the submitted dropdown value for the selected role, e.g., "GE"
+	GraderConfirmed string // GraderConfirmed is set only if Grader and faculty clicks graderReqMet
 
 	// Derived fields
-	User   *user.User     // User gets populated with the faculty member who is logged in
-	Course *course.Course // Course gets the course by looking up the faculty member + CRN
-	GE     *person.Person // GE is the person being set up as a GE, after looking up the DuckID
+	User           *user.User     // User gets populated with the faculty member who is logged in
+	Course         *course.Course // Course gets the course by looking up the faculty member + CRN
+	Agent          *person.Person // Agent is the person being assigned a role, after looking up the DuckID
+	GraderConfReqd string         // GraderConfReqd is "1" when role == Grader; for displaying the confirm checkbox
 
 	// Internal data
 	errors []error // errors will be populated with anything preventing the form submission, e.g., a bad duckid
