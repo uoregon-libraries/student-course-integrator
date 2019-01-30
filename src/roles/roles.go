3448919bd2c05c9cfb8e5671e5d931c9602495ad
lsat12357 <lsato@uoregon.edu>
2019-01-30 07:31:44 -0800

    roles: add package

--- src/roles/roles.go
+++ src/roles/roles.go
@@ -1,13 +1,13 @@
 package roles
 
-var Roles = [2]string{"GE", "Grader"}
+var Roles = []string{"GE", "Grader"}
 
 func validateRole(role string) bool {
 
-  for _, element := range Roles {
-    if role == element{
-      return true
-    }
-  }
-  return false
+	for _, element := range Roles {
+		if role == element {
+			return true
+		}
+	}
+	return false
 }
